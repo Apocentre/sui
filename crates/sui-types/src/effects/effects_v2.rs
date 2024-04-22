9621be8ae9f4a4d6608b22b57f41a1b9112672f8
ppoliani <ppoliani@gmail.com>
2024-04-22 15:26:15 +0300

    Revert "feat: make effects_v2 fields pub"
    This reverts commit 1b94de11d6f94a6fe6bbb5cf4af122153a180619.

--- crates/sui-types/src/effects/effects_v2.rs
+++ crates/sui-types/src/effects/effects_v2.rs
@@ -24,35 +24,35 @@ use std::collections::HashSet;
 #[derive(Eq, PartialEq, Clone, Debug, Serialize, Deserialize)]
 pub struct TransactionEffectsV2 {
     /// The status of the execution
-    pub status: ExecutionStatus,
+    status: ExecutionStatus,
     /// The epoch when this transaction was executed.
-    pub executed_epoch: EpochId,
-    pub gas_used: GasCostSummary,
+    executed_epoch: EpochId,
+    gas_used: GasCostSummary,
     /// The transaction digest
-    pub transaction_digest: TransactionDigest,
+    transaction_digest: TransactionDigest,
     /// The updated gas object reference, as an index into the `changed_objects` vector.
     /// Having a dedicated field for convenient access.
     /// System transaction that don't require gas will leave this as None.
-    pub gas_object_index: Option<u32>,
+    gas_object_index: Option<u32>,
     /// The digest of the events emitted during execution,
     /// can be None if the transaction does not emit any event.
-    pub events_digest: Option<TransactionEventsDigest>,
+    events_digest: Option<TransactionEventsDigest>,
     /// The set of transaction digests this transaction depends on.
-    pub dependencies: Vec<TransactionDigest>,
+    dependencies: Vec<TransactionDigest>,
 
     /// The version number of all the written Move objects by this transaction.
-    pub lamport_version: SequenceNumber,
+    lamport_version: SequenceNumber,
     /// Objects whose state are changed in the object store.
-    pub changed_objects: Vec<(ObjectID, EffectsObjectChange)>,
+    changed_objects: Vec<(ObjectID, EffectsObjectChange)>,
     /// Shared objects that are not mutated in this transaction. Unlike owned objects,
     /// read-only shared objects' version are not committed in the transaction,
     /// and in order for a node to catch up and execute it without consensus sequencing,
     /// the version needs to be committed in the effects.
-    pub unchanged_shared_objects: Vec<(ObjectID, UnchangedSharedKind)>,
+    unchanged_shared_objects: Vec<(ObjectID, UnchangedSharedKind)>,
     /// Auxiliary data that are not protocol-critical, generated as part of the effects but are stored separately.
     /// Storing it separately allows us to avoid bloating the effects with data that are not critical.
     /// It also provides more flexibility on the format and type of the data.
-    pub aux_data_digest: Option<EffectsAuxDataDigest>,
+    aux_data_digest: Option<EffectsAuxDataDigest>,
 }
 
 impl TransactionEffectsAPI for TransactionEffectsV2 {
